7557fd5687f74a429c079114dcbcc7f316d1c9a3
EduardoSantanaSeverino <eduardoamparos@gmail.com>
2023-09-28 22:35:36 -0400

    Add migration

--- source/MyCollegeV2/aspnet-core/src/MyCollegeV2.EntityFrameworkCore/EntityFrameworkCore/MyCollegeV2DbContextConfigurer.cs
+++ source/MyCollegeV2/aspnet-core/src/MyCollegeV2.EntityFrameworkCore/EntityFrameworkCore/MyCollegeV2DbContextConfigurer.cs
@@ -7,12 +7,12 @@ namespace MyCollegeV2.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<MyCollegeV2DbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            builder.UseNpgsql(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<MyCollegeV2DbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection);
+            builder.UseNpgsql(connection);
         }
     }
 }
--- source/MyCollegeV2/aspnet-core/src/MyCollegeV2.EntityFrameworkCore/Migrations/20230929020514_InitialMigration.Designer.cs
+++ source/MyCollegeV2/aspnet-core/src/MyCollegeV2.EntityFrameworkCore/Migrations/20230929020514_InitialMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyCollegeV2.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MyCollegeV2.Migrations
 {
     [DbContext(typeof(MyCollegeV2DbContext))]
-    partial class MyCollegeV2DbContextModelSnapshot : ModelSnapshot
+    [Migration("20230929020514_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
